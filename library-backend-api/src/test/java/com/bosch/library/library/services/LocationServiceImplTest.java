64173ed069ca5d74bb9e30abf0d93c22a55f07a7
Elitsa Dimitrova <die2sf@bosch.com>
2023-07-20 10:31:05 +0300

    Add unit tests for updating location

--- library-backend-api/src/test/java/com/bosch/library/library/services/LocationServiceImplTest.java
+++ library-backend-api/src/test/java/com/bosch/library/library/services/LocationServiceImplTest.java
@@ -27,6 +27,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -93,5 +94,35 @@ public class LocationServiceImplTest {
 
         // Verify that it is saved
         verify(this.locationRepository, times(1)).save(location);
+    }
+
+    @Test
+    void testUpdateLocationUpdatesAddressInfo() throws ElementNotFoundException {
+        // Arrange mock repository
+        final Location location = this.locationList.get(0);
+        Mockito.when(this.locationRepository.findById(1L)).thenReturn(Optional.of(location));
+        Mockito.when(this.locationRepository.save(any(Location.class))).thenReturn(location);
+
+        // Change location's data
+        final LocationDTO updatedLocation = new LocationDTO(1L, "new location");
+        final LocationDTO result = this.locationService.updateLocation(updatedLocation);
+
+        // Assert that all new data is saved and valid
+        verify(this.locationRepository, times(1)).save(location);
+        assertEquals(updatedLocation.getAddress(), result.getAddress());
+    }
+
+    @Test
+    void testUpdateLocationThrowsOnInvalidLocationId() {
+        // Arrange mock repository
+        Mockito.when(this.locationRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // Assert that updating nonexistent location throws exception
+        final LocationDTO updatedLocation = new LocationDTO(1L, "new location");
+        assertThrows(
+                ElementNotFoundException.class,
+                () -> this.locationService.updateLocation(updatedLocation),
+                "Updating location's data should throw ElementNotFoundException when location optional is empty."
+        );
     }
 }
